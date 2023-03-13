e1afad0bf831a69183315924a93bf587cb506e0f
SajaAhmad <saja-a7@hotmail.com>
2023-03-13 21:21:05 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://iris:'+ encodeURIComponent('090909') + '@cluster0.kk6jx.mongodb.net/?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://iris:'+ encodeURIComponent('') + '@cluster0.kk6jx.mongodb.net/?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
