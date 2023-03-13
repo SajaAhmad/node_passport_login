8dec7d11eec8bc45193c5048ea3a71f46a79789d
SajaAhmad <saja-a7@hotmail.com>
2023-03-13 21:10:40 +0300

    test

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+dbPassword = 'mongodb+srv://iris:'+ encodeURIComponent('090909') + '@cluster0.kk6jx.mongodb.net/?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
